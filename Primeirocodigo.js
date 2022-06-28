579fb7de47a2a9812252f49353684961d42f9f4a
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-06-28 09:40:49 -0300

    Exemplos da aula 1

---
+++ Primeirocodigo.js
@@ -0,0 +1,18 @@
+const numero1 = 1;
+const numero2 = 1;
+const soma = numero1+numero2;
+const texto = "Meu nome é: ";
+const nome = "Yuri";
+
+console.log(soma);
+console.log(texto + nome);
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
