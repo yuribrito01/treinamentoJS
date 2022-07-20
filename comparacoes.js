fa2ae0de47d415bc825f0a962380e8d09b60f687
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-07-20 13:44:10 -0300

    Adicionando codigo de comparaçoes

---
+++ comparacoes.js
@@ -0,0 +1,12 @@
+const numero = 5;
+const string = "5";
+
+//Compara o valor e o tipo de dado
+console.log(numero === string);
+
+//Apresentar o tipo de cada uma
+console.log(typeof numero);
+console.log(typeof string);
+
+//So compara o valor
+
