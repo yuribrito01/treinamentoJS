529ecd20d32c5b1464e5516a092b9273108b8073
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-07-20 11:58:20 -0300

    Estudando consoles

---
+++ consoles.js
@@ -0,0 +1,9 @@
+const minhaVar = true;
+
+console.log(234);
+console.log("Eu sou um texto");
+console.log(minhaVar);
+
+//Tratamentos de erro!
+console.log("Deu erro!");
+console.error(new Error("deu erro"));
