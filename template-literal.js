2b5fbb460118a43c330fada328314ad3299e9825
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-07-20 14:34:17 -0300

    Aprendendo ultilizar o template literal.

---
+++ template-literal.js
@@ -0,0 +1,13 @@
+const nome = "Yuri";
+const idade = 20;
+const cidade = "Campinas";
+
+//const apresentacao = "Meu nome é " + nome + ", minha idade é: " + idade + " Anos. Nasci na cidade de " + cidade; 
+
+//Utilizando o template literal
+
+const apresentacao =  `Meu nome e ${nome}, minha idade e: ${idade} Anos. Nasci na cidade de  ${cidade}`;
+
+//Obs: Utilizar crase. 
+
+console.log(apresentacao);
---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// if(idadeCliente>=idadeMinima){
+//     console.log("Ceverja");
+// }else{
+//     console.log("Suco");
+// }
+
+//Utilizando ternário
+console.log(idadeCliente>=idadeMinima ? "Ceverja" : "Suco");
