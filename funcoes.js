a861053d643be4e1713da8ca6d3b9afe6465dae7
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-07-21 12:08:12 -0300

    Adicionando o arquivo funcoes

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+let numero = 4.3;
+
+console.log(Math.round(numero));
+
+console.log(Math.trunc(numero));
+
+console.log(Math.sqrt(numero));
+
+console.log(Math.min(0, 150, 30, 20, -8, -200));
+
+console.log(Math.max(0, 150, 30, 20, -8, -200));
