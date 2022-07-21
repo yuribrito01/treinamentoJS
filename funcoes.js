829845d89caff24cf268cfbe7bd2e728ef2932d0
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2022-07-21 14:04:47 -0300

    Adicionando duas funcoes (soma e multiplicacao)

--- funcoes.js
+++ funcoes.js
@@ -9,3 +9,13 @@ console.log(Math.sqrt(numero));
 console.log(Math.min(0, 150, 30, 20, -8, -200));
 
 console.log(Math.max(0, 150, 30, 20, -8, -200));
+
+function soma(num1, num2){
+    return num1+num2;
+}
+
+function multiplicacao(a, b){
+    return a*b;
+}
+
+console.log(multiplicacao(soma(2,2), soma(1,1)));
